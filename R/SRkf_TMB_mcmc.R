99ff3bea0920eadfe24bfa49b0b867eb1ae0db24
Wor <WorC@ENT.dfo-mpo.ca>
2018-05-11 09:49:17 -0700

    graph routines

--- R/SRkf_TMB_mcmc.R
+++ R/SRkf_TMB_mcmc.R
@@ -7,13 +7,14 @@
 #load in required packages and libraries
 library(ggplot2)
 library(TMB)
-library(TMBhelper)
+#library(TMBhelper)
 library(bayesplot)
 library(tmbstan)
+library(reshape)
 
 #load in directories.R
-#source("C:/Users/worc/Documents/HarrisonSR/R/directories.R")
-source("/Users/catarinawor/Documents/work/Chinook/srkf/R/directories.R")
+source("C:/Users/worc/Documents/HarrisonSR/R/directories.R")
+#source("/Users/catarinawor/Documents/work/Chinook/srkf/R/directories.R")
 
 #read in simple data set
 setwd(data_dir)
@@ -33,9 +34,10 @@ df<-df[order(sort(SR$S_adj)),]
 #kalman filter model
 mydata<-list(obs_logR=log(SR$R),obs_S=SR$S_adj)
 setwd(model_dir)
-compile("Rickerkf_ratiovar.cpp")
+compile("Rickerkf_ratiovar.cpp",libtmb=FALSE, "-O1 -g", DLLFLAGS="")
 
-dyn.load("Rickerkf_ratiovar.so")
+dyn.load(dynlib("Rickerkf_ratiovar"))
+
 
 
 data <- mydata
@@ -59,7 +61,7 @@ repkf<-objkf$report()
 repkf
 
 
-TMBAIC(optkf, p = 2, n = Inf)
+#TMBAIC(optkf, p = 2, n = Inf)
 predR2<-matrix(NA,nrow=length(SR$S_adj),ncol=length(repkf$alpha))
 
 for(i in 1:length(repkf$alpha)){
@@ -76,7 +78,7 @@ df<-data.frame(S=rep(SR$S_adj,length(repkf$alpha)),
 		BroodYear=rep(SR$BroodYear,length(repkf$alpha)))
 
 
-df<-df[sort(order(df$SR)),]
+
 
 ###################
 #MCMC
@@ -100,16 +102,35 @@ stan_diag(fitmcmc2,
             chain = 0)
 
 
+
+
 fit_summary <- summary(fitmcmc2)
 fit_summary$summary
 
 posterior <- as.array(fitmcmc2)
 dim(posterior)
 
+head(posterior[,1,2])
 
-mcmc_dens_overlay(posterior, pars =  c(names(objkf$par)))
-mcmc_pairs(posterior, pars =  c(names(objkf$par)),
-           off_diag_args = list(size = 1.5))
+posterior[1:3,,]
+
+
+mainrun<-melt(posterior)
+head(mainrun)
+summary(mainrun)
+mainrun$type="posdata"
+mainrun$type<-as.factor(mainrun$type)
+
+
+pm<-ggplot(mainrun)
+pm<-pm+geom_density(aes(x=value, color=chains))
+pm<-pm+facet_wrap(~parameters, scales="free")
+pm
+?density
+
+#mcmc_dens_overlay(posterior, pars =  c(names(objkf$par)))
+#mcmc_pairs(posterior, pars =  c(names(objkf$par)),
+#          off_diag_args = list(size = 1.5))
 
 
 #==================================================================
@@ -120,7 +141,7 @@ mcmc_pairs(posterior, pars =  c(names(objkf$par)),
 setwd(model_dir)
 compile("Rickerkf_ratiovar_predata.cpp")
 
-dyn.load("Rickerkf_ratiovar_predata.so")
+dyn.load(dynlib("Rickerkf_ratiovar_predata"))
 
 data <- mydata
 
@@ -158,6 +179,69 @@ dim(mc)
 posteriorpd <- as.array(fitmcmc2pd)
 dim(posteriorpd)
 head(posteriorpd)
+
+predata<-melt(posteriorpd)
+predata$type="predata"
+predata$type<-as.factor(predata$type)
+
+
+allruns<-rbind(predata,mainrun)
+
+
+myparam<-unique(allruns$parameters)
+
+
+
+for(i in 1:length(myparam)){
+  
+
+  if(myparam[i]=="logbeta"){
+    resalpha<-allruns[allruns$parameters==myparam[i],]
+
+    pao<-ggplot(resalpha)
+    pao<-pao+geom_density(aes(x=exp(value), color=type,fill=type),alpha=0.5)
+    pao<-pao+xlab("beta")
+    print(pao)
+
+    setwd(figs_dir)
+
+    ggsave(paste("pmpd","beta",".pdf", sep=""), plot = pao, width = 7, height = 7, units = "cm")
+    
+    pao<-ggplot(resalpha)
+    pao<-pao+geom_density(aes(x=1/exp(value), color=type,fill=type),alpha=0.5)
+    pao<-pao+xlab("Smax")
+    print(pao)
+
+    setwd(figs_dir)
+
+    ggsave(paste("pmpd_gamma","Smax",".pdf", sep=""), plot = pao, width = 10, height = 10, units = "cm")
+    
+
+  }else{
+
+
+  resalpha<-allruns[allruns$parameters==myparam[i],]
+
+  pao<-ggplot(resalpha)
+  pao<-pao+geom_density(aes(x=value, color=type,fill=type),alpha=0.5)
+  pao<-pao+xlab(myparam[i])
+  print(pao)
+
+  setwd(figs_dir)
+
+  ggsave(paste("pmpd_gamma",myparam[i],".pdf", sep=""), plot = pao, width = 10, height = 10, units = "cm")
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 mcmc_dens_overlay(posteriorpd, pars =  c(names(objkfpd$par)))
